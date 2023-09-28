6bad34f6129e5038ff2fa3be905628f07124a1f8
sepidsho <144679518+sepidsho@users.noreply.github.com>
2023-09-28 09:26:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Person
+{
+    private int age;
+    private string firstName;
+    private string lastName;
+    private double height;
+    private double weight;
+    
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (v)
+        }
+      
+
+    }
+
+}
